a6a23b26e81060bd3d65c591bbc05a097381b133
Harun Reşit KARAHAN <harun_artin@hotmail.com>
2024-06-14 11:36:18 +0300

    .

--- webapp/controller/InitialScreen.controller.js
+++ webapp/controller/InitialScreen.controller.js
@@ -2,25 +2,41 @@ sap.ui.define([
     "./BaseController",
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
+    'sap/ui/core/Fragment',
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator"
-], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+], function (BaseController, JSONModel, formatter, Fragment, Filter, FilterOperator) {
     "use strict";
 
     return BaseController.extend("com.martur.zmbdocumentmanagment.controller.InitialScreen", {
 
         formatter: formatter,
+        onInit: function () {
 
-        /* =========================================================== */
-        /* lifecycle methods                                           */
-        /* =========================================================== */
-
-        /**
-         * Called when the worklist controller is instantiated.
-         * @public
-         */
-        onInit : function () {
-
+        },
+        _openDialog: function (dialogName) {
+            if (!this._oDialog) {
+                Fragment.load({
+                    name: `com.martur.zmbdocumentmanagment.fragment.${dialogName}`,
+                    controller: this
+                }).then(
+                    function (oDialog) {
+                        // connect dialog to the root view of this component (models, lifecycle)
+                        this.getView().addDependent(oDialog);
+                        oDialog.addStyleClass(
+                            this.getOwnerComponent().getContentDensityClass()
+                        );
+                        oDialog.open(oDialog);
+                    }.bind(this));
+            }
+        },
+        _closeDialog: function () {
+            // this._oDialog.close();
+            // this._oDialog.destroy();
+            this._oDialog = undefined;
+        },
+        _documentSortConfirm: function (oEvent) {
+            this._closeDialog()
         }
     });
 });
