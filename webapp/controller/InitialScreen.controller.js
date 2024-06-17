293bb9d73202b88a0152fd8285c01faed54de24a
Harun Karahan <harun_artin@hotmail.com>
2024-06-17 12:27:49 +0300

    .

--- webapp/controller/InitialScreen.controller.js
+++ webapp/controller/InitialScreen.controller.js
@@ -10,10 +10,10 @@ sap.ui.define(
     "sap/ui/model/type/String",
     "sap/ui/table/Column",
     "sap/ui/table/TreeTable",
-    'sap/m/ColumnListItem',
+    "sap/m/ColumnListItem",
     "sap/m/SearchField",
-    'sap/m/Column',
-    'sap/m/Text'
+    "sap/m/Column",
+    "sap/m/Text",
   ],
   function (
     BaseController,
@@ -100,7 +100,7 @@ sap.ui.define(
                 oDialog.getTableAsync().then(
                   function (oTable) {
                     oTable.setModel(this.getModel("jsonModel"));
-                    
+
                     // For Desktop and tabled the default table is sap.ui.table.Table
                     if (oTable.bindRows) {
                       // Bind rows to the ODataModel and add columns
@@ -113,19 +113,19 @@ sap.ui.define(
                         },
                       });
                       oColumnDocumentCode = new UIColumn({
-                        width: '8rem',
+                        width: "8rem",
                         label: new Label({ text: "Döküman No" }),
-                        template:  new Text({
-                            wrapping: false,
-                            text: `{key}`
-                          })
+                        template: new Text({
+                          wrapping: false,
+                          text: `{key}`,
+                        }),
                       });
                       oColumnDocumentName = new UIColumn({
                         label: new Label({ text: "Döküman Ad" }),
                         template: new Text({
-                            wrapping: false,
-                            text: `{fileName}`
-                          })
+                          wrapping: false,
+                          text: `{fileName}`,
+                        }),
                       });
                       oTable.addColumn(oColumnDocumentCode);
                       oTable.addColumn(oColumnDocumentName);
@@ -168,56 +168,89 @@ sap.ui.define(
           }
         },
         onDocumentNameValueHelpOkPress: function (oEvent) {
-			var aTokens = oEvent.getParameter("tokens");
-			if (aTokens.length > 0) {
-				this.getModel("jsonModel").setProperty("/filterInputValues/documentName", aTokens[0].getText());
-			}
-			this._closeDialog();
-		},
+          var aTokens = oEvent.getParameter("tokens");
+          if (aTokens.length > 0) {
+            this.getModel("jsonModel").setProperty(
+              "/filterInputValues/documentName",
+              aTokens[0].getText()
+            );
+          }
+          this._closeDialog();
+        },
         onDocumentNameFilterBarSearch: function (oEvent) {
-			var sSearchQuery = this._oBasicSearchField.getValue(),
-				aSelectionSet = oEvent.getParameter("selectionSet");
+          var sSearchQuery = this._oBasicSearchField.getValue(),
+            aSelectionSet = oEvent.getParameter("selectionSet");
 
-			var aFilters = aSelectionSet && aSelectionSet.reduce(function (aResult, oControl) {
-				if (oControl.getValue()) {
-					aResult.push(new Filter({
-						path: oControl.getName(),
-						operator: FilterOperator.Contains,
-						value1: oControl.getValue()
-					}));
-				}
+          var aFilters =
+            aSelectionSet &&
+            aSelectionSet.reduce(function (aResult, oControl) {
+              if (oControl.getValue()) {
+                aResult.push(
+                  new Filter({
+                    path: oControl.getName(),
+                    operator: FilterOperator.Contains,
+                    value1: oControl.getValue(),
+                  })
+                );
+              }
 
-				return aResult;
-			}, []);
+              return aResult;
+            }, []);
 
-			aFilters.push(new Filter({
-				filters: [
-					// new Filter({ path: "key", operator: FilterOperator.Contains, value1: sSearchQuery }),
-					new Filter({ path: "fileName", operator: FilterOperator.Contains, value1: sSearchQuery })
-				],
-				and: false
-			}));
+          aFilters.push(
+            new Filter({
+              filters: [
+                // new Filter({ path: "key", operator: FilterOperator.Contains, value1: sSearchQuery }),
+                new Filter({
+                  path: "fileName",
+                  operator: FilterOperator.Contains,
+                  value1: sSearchQuery,
+                }),
+              ],
+              and: false,
+            })
+          );
 
-			this._filterTable(new Filter({
-				filters: aFilters,
-				and: true
-			}));
-		},
+          this._filterTable(
+            new Filter({
+              filters: aFilters,
+              and: true,
+            })
+          );
+        },
         _filterTable: function (oFilter) {
-			var oVHD = this._oVHD;
+          var oVHD = this._oVHD;
 
-			oVHD.getTableAsync().then(function (oTable) {
-				if (oTable.bindRows) {
-					oTable.getBinding("rows").filter(oFilter);
-				}
-				if (oTable.bindItems) {
-					oTable.getBinding("items").filter(oFilter);
-				}
+          oVHD.getTableAsync().then(function (oTable) {
+            if (oTable.bindRows) {
+              oTable.getBinding("rows").filter(oFilter);
+            }
+            if (oTable.bindItems) {
+              oTable.getBinding("items").filter(oFilter);
+            }
 
-				// This method must be called after binding update of the table.
-				oVHD.update();
-			});
-		}
+            // This method must be called after binding update of the table.
+            oVHD.update();
+          });
+        },
+        onTreeTableBasicSearch: function (oEvent) {
+          let table = this.getView().byId("idDocumentTreeTable"),
+            oBinding = table.getBinding("rows"),
+            oFilter = [],
+            inputValue = oEvent.getSource().getValue();
+          if (inputValue !== "") {
+            oFilter = new Filter(
+              [
+                new Filter("fileName", FilterOperator.Contains, inputValue),
+                new Filter("uploadedBy", FilterOperator.Contains, inputValue),
+              ],
+              false
+            );
+            oBinding.filter([oFilter]);
+          } else {
+            oBinding.filter([oFilter]);
+          }
+        },
       }
     );
   }
